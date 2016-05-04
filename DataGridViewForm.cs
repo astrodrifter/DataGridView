efda75be0f12ddb82ae410e152d2e30ee6b9ffc3
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2016-05-04 22:33:42 +1000

    Adding solution
    from zip

---
+++ DataGridViewForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataViewGrid_Example {
+
+    public partial class DataGridViewForm : Form {
+
+        BindingList<Player> players = new BindingList<Player>();
+
+        private int numberofPlayers = 0;
+        private const int MAX_PLAYERS = 10;
+
+        private string[] defaultNames = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" };
+
+        /// <summary>
+        /// The form's constructor.
+        /// </summary>
+        public DataGridViewForm() {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Event-handler for when the "Add player" button is clicked.
+        /// </summary>
+        private void addPlayerButton_Click(object sender, EventArgs e) {
+
+            if (numberofPlayers < MAX_PLAYERS) {
+                players.Add(new Player(defaultNames[numberofPlayers]));
+                numberofPlayers += 1;
+                //RefreshPlayersInfoInDataGridView();
+            } else {
+                MessageBox.Show("No more players allowed.");
+            }
+        } // end addPlayerButton_Click
+
+        /// <summary>
+        /// Event-handler for when the "Add player" button is clicked.
+        /// </summary>
+        private void showValuesButton_Click(object sender, EventArgs e) {
+            string message = "";
+            foreach (Player player in players) {
+                message += player.Name + "\t" + player.GamePoints + "\t" + player.HighestPoints + "\n";
+            }
+            MessageBox.Show(message);
+
+        } // end showValuesButton_Click
+
+        /// <summary>
+        /// Closes the form whenever the Escape key is pressed.  You can ignore this code.
+        /// </summary>
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData) {
+            if (keyData == Keys.Escape) {
+                Close();
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        } // end ProcessCmdKey
+
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Drawing;   // For Color.
+
+namespace DataViewGrid_Example {
+
+    class Player {
+
+        private Random randomNumber = new Random();
+
+        // The first property: the player's name.
+        private string name;
+        public  string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        // The second property: the player's points in the current game.
+        private int gamePoints;
+        public  int GamePoints {
+            get { return gamePoints; }
+            set { gamePoints = value; }
+        }
+
+        // The third property: the player's highest points in all the games.
+        private int highestPoints;
+        public  int HighestPoints {
+            get { return highestPoints; }
+            set { highestPoints = value; }
+        }
+
+        // The fourth property: the player's colour.
+        private Color playerTokenColour;
+        public Color PlayerTokenColour {
+            get { return playerTokenColour; }
+            set {
+                playerTokenColour = value;
+                SolidBrush brush = new SolidBrush(playerTokenColour);
+                playerTokenImage = new Bitmap(1, 1);
+                using (Graphics g = Graphics.FromImage(PlayerTokenImage)) {
+                    g.FillRectangle(brush, 0, 0, 1, 1);
+                }
+            }
+        }
+
+        // The fifth property: a DataGridView cannot display a Color directly,
+        // but it can display an Image. So, we have a PlayerTokenImage that is
+        // set by to match the PlayerTokenColour.
+        private Image playerTokenImage;
+        public Image PlayerTokenImage {
+            get { return playerTokenImage; }
+        }
+
+        /// <summary>
+        /// The constructor.
+        /// </summary>
+        public Player(string name) {
+            // Initialise the player. We use some random values to simulate those in a real game.
+            this.name = name;
+            this.gamePoints = randomNumber.Next(0, 100);
+            this.highestPoints = randomNumber.Next(0, 1000);
+            if (highestPoints < gamePoints) {
+                highestPoints = gamePoints;
+            }
+            this.PlayerTokenColour = GenerateARandomColour();
+        }
+
+        /// <summary>
+        /// Generates a randomly-chosen colour.
+        /// </summary>
+        /// <returns>A random colour</returns>
+        private Color GenerateARandomColour() {
+            const int HIGHEST_COLOUR_VALUE = 255;
+            int red = randomNumber.Next(0, HIGHEST_COLOUR_VALUE);
+            int green = randomNumber.Next(0, HIGHEST_COLOUR_VALUE);
+            int blue = randomNumber.Next(0, HIGHEST_COLOUR_VALUE);
+            Color randomColor = Color.FromArgb(red, green, blue);
+            return randomColor;
+        }
+    }
+}
